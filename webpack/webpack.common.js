cf59636ac81a87a165edeb6d93b5faa90896b789
MikRyam <mikhailryamov@gmail.com>
2023-02-26 10:27:08 +0300

    update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // mode: "development",
   output: {
     path: path.resolve(__dirname, "..", "./build"),
     filename: "bundle.js",
@@ -51,13 +50,4 @@ module.exports = {
       template: path.resolve(__dirname, "..", "./src/index.html"),
     }),
   ],
-  // devtool: "inline-source-map",
-  // devServer: {
-  //   static: path.join(__dirname, "build"),
-  //   historyApiFallback: true,
-  //   port: 3000,
-  //   open: true,
-  //   hot: true,
-  //   host: 'localhost',
-  // },
 }
